3dc0132c21121423de265c8b99852cef5418265b
Kim Mattiko <amberlie@rocketmail.com>
2015-06-10 09:01:14 -0400

    getting final touches in

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,12 @@
   //$('tabs').removeClass("current");
   //$(this).addClass("current");
 
-$.getJSON('../../api/github/users/octocat/profile.json')
+$.getJSON('../api/github/users/octocat/profile.json')
   .then(function(octocat){
       //puts information to console?
     console.log(octocat);
       //unique name tied back to where in html(Dave's class example)
-    $mainName = $('.profile#name');
-      //where is the item in the json file?
-    $mainName.attr(octocat.name);
-
-    $mainName.text(octocat.login);
+    $mainName = $('h3#name');
+    $mainName.text(octocat.name);
 //something is not quite right with this code, no change.
  });
