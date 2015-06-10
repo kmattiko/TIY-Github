8aa58752e8ba8decafb9729a998842257a1fe4e0
Kim Mattiko <amberlie@rocketmail.com>
2015-06-09 21:08:25 -0400

    attempted some JS without anything working

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
-$(".tabs").click()
-  $('tabs').removeClass("current");
-  $(this).addClass("current");
+//$(".tabs").click()
+  //$('tabs').removeClass("current");
+  //$(this).addClass("current");
+
+$.getJSON('../../api/github/users/octocat/profile.json')
+  .then(function(octocat){
+      //puts information to console?
+    console.log(octocat);
+      //unique name tied back to where in html(Dave's class example)
+    $mainName = $('.profile#name');
+      //where is the item in the json file?
+    $mainName.attr(octocat.name);
+
+    $mainName.text(octocat.login);
+//something is not quite right with this code, no change.
+ });
