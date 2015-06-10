62fc54f7dff6da428a6fd34306e3e0560a182b19
Kim Mattiko <amberlie@rocketmail.com>
2015-06-10 18:58:15 -0400

    got cats and cats to live together

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,10 @@
 
 $.getJSON('/../../api/github/users/octocat/profile.json')
   .then(function(octocat){
-      //puts information to console?
     console.log(octocat);
-      //unique name tied back to where in html(Dave's class example)
-    $mainName = $('h3.name');
+
+    $mainName = $('#profileName');
     $mainName.text(octocat.name);
-//something is not quite right with this code, no change.
+
+
  });
