b2ae2b22d8bfb0f9ab0284b8a1d87df5cf63c17d
Kim Mattiko <amberlie@rocketmail.com>
2015-06-10 14:20:40 -0400

    commit what we did in class together

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,12 @@
   //$('tabs').removeClass("current");
   //$(this).addClass("current");
 
-$.getJSON('../api/github/users/octocat/profile.json')
+$.getJSON('/../../api/github/users/octocat/profile.json')
   .then(function(octocat){
       //puts information to console?
     console.log(octocat);
       //unique name tied back to where in html(Dave's class example)
-    $mainName = $('h3#name');
+    $mainName = $('h3.name');
     $mainName.text(octocat.name);
 //something is not quite right with this code, no change.
  });
