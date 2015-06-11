c166ad9220951a2681b8a18be34e23b740b43e5d
Kim Mattiko <amberlie@rocketmail.com>
2015-06-10 21:59:01 -0400

    got octocats profile information loaded with JQuery

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,24 @@ $.getJSON('/../../api/github/users/octocat/profile.json')
   .then(function(octocat){
     console.log(octocat);
 
+    $profilePic = $('#profilePic');
+    $profilePic.attr('src', octocat.avatar_url);
+
     $mainName = $('#profileName');
     $mainName.text(octocat.name);
 
+    $logins = $('#logins');
+    $logins.text(octocat.login);
 
+    $company = $('#company');
+    $company.text(octocat.company);
+
+    $location = $('#location');
+    $location.text(octocat.location);
+
+    $email = $('#email');
+    $email.text(octocat.email);
+
+    $blog = $('#blog');
+    $blog.text(octocat.blog);
  });
